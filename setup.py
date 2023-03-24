53fc0c49ca40e36cf9000b99c6881117c31c47dd
Heitor gessner <hmynssen@gmail.com>
2023-03-24 17:23:25 -0300

    setup text for pip

--- setup.py
+++ setup.py
@@ -1,34 +1,50 @@
-'''<h2>Aesthetic for Matplotlib and Python Framework for data analysis</h2>
+'''
+===============================================================
+Aesthetic for Matplotlib and Python Framework for data analysis
+===============================================================
 
-<h3>Aesthetics</h3>
+Aesthetics
+==========
 
 To use only the aesthetics and not the framework simply use create a class instance and call for PlotLikeR().plot_cfg() and/or PlotLikeR().plot_cfg_grid() methods.
 
-In the matplotlib documentation it is possible to find a description of each individual parameter. The majority of which, in the Aesplot library, are set as the default. For more check <a href="https://matplotlib.org/stable/tutorials/introductory/customizing.html#the-default-matplotlibrc-file">this link</a>.
+In the matplotlib documentation it is possible to find a description of each individual parameter. The majority of which, in the Aesplot library, are set as the default. For more check this link: https://matplotlib.org/stable/tutorials/introductory/customizing.html#the-default-matplotlibrc-file .
 
-<h3>Framework</h3>
+Framework
+=========
+
 Use a separate python file to create the basic framework structure.
 Modify the new file or string to make your analysis.
 
-<h3>Utils</h3>
-</h5>Fixing the SVG export from matplotlib</h5>
+Utils
+=====
+
+Fixing the SVG export from matplotlib
+-------------------------------------
+
 When saving the matplotlib figure as SVG, the syntax used doesn't contain any &lt;text&gt;&lt;/text&gt;. Instead, matplotlib opts for creating there own letters as mathematical curves. This can be an issue due to the "low resolution" aspect of these curves. To solve this, an utility tool was developed to find and replace the curves for letters.
 
 Additionally, when using LaTex equations in labels and/or legends, some elements can also be converted into HTML syntax.
 
-<h5>Converting LaTex to HTML</h5>
+Converting LaTex to HTML
+------------------------
+
 Some mathematical notation is usually used when plotting graphs such as square root, superscript, index and many others. And, since matplotlib supports LaTex interpretation/compilation, it is useful to have a translation from one markup language to another.
 
 Using utils.SVGText() replaces markups, such as /textbf{}, into an equivalent &lt;tspan&gt;&lt;/tspan&gt; around the tag. More LaTex markups will be added in the future.
 
-<h3>Dependencies</h3>
-  - <a href="https://numpy.org">Numpy</a>
-  - <a href="https://matplotlib.org">Matplotlib</a>
-  - <a href="https://pandas.pydata.org">Pandas</a>
-  - <a href="https://scipy.org">Scipy</a>
-  - <a href="https://miktex.org">Miktex</a> or other latex compiler
+Dependencies
+------------
 
-<h3>To-dos</h3>
+  - Numpy: https://numpy.org
+  - Matplotlib: https://matplotlib.org
+  - Pandas: https://pandas.pydata.org
+  - Scipy: https://scipy.org
+  - Miktex: https://miktex.org or other latex compiler
+
+To-dos
+------
+
   - Expand the latex functions that can be interpreted and translated
   - Develop tests
   - Create data examples and plot examples
@@ -38,8 +54,8 @@ Using utils.SVGText() replaces markups, such as /textbf{}, into an equivalent &l
 from setuptools import setup, find_packages
 ##python setup.py sdist bdist_wheel
 ##twine upload dist/*
-VERSION = '1.0'
-DESCRIPTION = 'Aesplot - Aesthetics and Framework'
+VERSION = '1.01'
+DESCRIPTION = 'Aesthetics and Framework'
 LONG_DESCRIPTION = __doc__
 setup(
         name="aesplot",
